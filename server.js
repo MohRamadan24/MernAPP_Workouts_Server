ef8c128cf0ecc48c9d108d1699be51d12fc56649
MOH. RAMADAN <69750539+MohRamadan24@users.noreply.github.com>
2024-06-01 19:07:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const cors = require('cors')
 
 // express app
 const app = express()
+
+// cors
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
 
 // middleware
 app.use(express.json())
